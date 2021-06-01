366a1ed54e52bf0830beca44213bea88df64085c
mccleskeyc <mccleskeyc@gmail.com>
2021-06-01 14:07:45 -0400

    initial component and module set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+//Decorator
+@Component({ 
+  selector: 'app-root', //first thing that runs when we run the page, a tag in our index.html
+  template: `
+    <h1>{{title}}</h1>
+  `
 })
-export class AppComponent {
-  title = 'learn-angular';
+
+export class AppComponent implements OnInit {
+  //OnInit provides an interface of rules
+  title: string | undefined;
+  constructor() { }
+
+  ngOnInit() {
+    //required for OnInit
+    //initializes component (title) with data
+    this.title = 'Hello World'
+  }
+  
 }
